ce51e1d8d534b61efef541455ef25fdda5cadf9e
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-23 23:14:49 +0300

    Add files via upload

---
+++ exampleconfig.py
@@ -0,0 +1,15 @@
+from sample_config import Config
+
+
+class Development(Config):
+    # ضـع اكـوادك تحت مكـان الكـلام العـربي  .. امسـح الكلام العربي وضع اكوادك
+    APP_ID = 6
+    API_HASH = "ضع كود الايبي هاش"
+    ALIVE_NAME = "اسم حسابك التلي"
+    DB_URI = "رابـط التخـزين الخـاص بك"
+    STRING_SESSION = "كود تيرمــكس"
+    TG_BOT_TOKEN = "توكـن البـوت الخـاص بك"
+    PRIVATE_GROUP_BOT_API_ID = -100
+    COMMAND_HAND_LER = "."
+    SUDO_COMMAND_HAND_LER = "،"
+    TZ = "Asia/Baghdad"
