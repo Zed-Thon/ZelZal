3a5846a5d86c70bba84a42bd300df548c792a454
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-26 18:10:41 +0300

    Update nsfw.py

--- zthon/assistant/nsfw.py
+++ zthon/assistant/nsfw.py
@@ -4,7 +4,7 @@ from telethon import Button
 from telethon.errors import MessageNotModifiedError
 from telethon.events import CallbackQuery
 
-from userbot import zedub
+from zthon import zedub
 
 from ..Config import Config
 from ..core.logger import logging
