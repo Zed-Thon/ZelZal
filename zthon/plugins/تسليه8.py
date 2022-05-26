56d0c0564db34abdda56fc96ff923a1c36f8f99e
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-26 17:54:48 +0300

    Update تسليه8.py

--- zthon/plugins/تسليه8.py
+++ zthon/plugins/تسليه8.py
@@ -7,8 +7,8 @@ import random
 from urllib.parse import quote_plus
 from collections import deque
 from telethon.tl.types import ChannelParticipantsAdmins
-from userbot.core.logger import logging
-from userbot import zedub
+from zthon.core.logger import logging
+from zthon import zedub
 from ..Config import Config
 from ..core.managers import edit_delete, edit_or_reply
 from . import ALIVE_NAME, deEmojify, mention
