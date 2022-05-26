56c1109559cb3565feacf1b372ef8fdf934e61b3
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-26 21:09:11 +0300

    Update afk.py

--- zthon/plugins/afk.py
+++ zthon/plugins/afk.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from telethon.tl import functions, types
 
-from userbot import zedub
+from zthon import zedub
 
 from ..Config import Config
 from ..core.logger import logging
