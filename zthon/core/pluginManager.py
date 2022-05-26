6ad867cd3159927ec01f45bd2029359575292829
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-26 20:21:38 +0300

    Update pluginManager.py

--- zthon/core/pluginManager.py
+++ zthon/core/pluginManager.py
@@ -76,7 +76,7 @@ async def restart_script(client: TelegramClient, sandy):
     except Exception as e:
         LOGS.error(e)
     executable = sys.executable.replace(" ", "\\ ")
-    args = [executable, "-m", "userbot"]
+    args = [executable, "-m", "zthon"]
     os.execle(executable, *args, os.environ)
     os._exit(143)
 
