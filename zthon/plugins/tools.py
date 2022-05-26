9605529ab870371526611c904a6f5821451bcfae
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-26 18:24:52 +0300

    Update tools.py

--- zthon/plugins/tools.py
+++ zthon/plugins/tools.py
@@ -14,7 +14,7 @@ from PIL import Image, ImageColor
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest as unblock
 
-from userbot import zedub
+from zthon import zedub
 
 from ..Config import Config
 from ..core.logger import logging
