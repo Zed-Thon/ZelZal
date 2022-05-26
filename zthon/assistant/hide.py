099942492191e23024e7218a1324dcb0d5161560
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-26 17:47:17 +0300

    Update hide.py

--- zthon/assistant/hide.py
+++ zthon/assistant/hide.py
@@ -10,8 +10,8 @@ from zthon import zedub
 @zedub.tgbot.on(CallbackQuery(data=re.compile(b"hide_(.*)")))
 async def on_plug_in_callback_query_handler(event):
     timestamp = int(event.pattern_match.group(1).decode("UTF-8"))
-    if os.path.exists("./userbot/hide.txt"):
-        jsondata = json.load(open("./userbot/hide.txt"))
+    if os.path.exists("./zthon/hide.txt"):
+        jsondata = json.load(open("./zthon/hide.txt"))
         try:
             reply_pop_up_alert = jsondata[f"{timestamp}"]["text"]
         except KeyError:
