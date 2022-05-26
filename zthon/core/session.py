b7cee1ea7b8581595d8d80851b9e36f7a97b2529
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-26 16:37:04 +0300

    Add files via upload

---
+++ zthon/core/session.py
@@ -0,0 +1,44 @@
+import os
+import sys
+
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+from telethon.sessions import StringSession
+
+from ..Config import Config
+from .client import ZedUserBotClient
+
+__version__ = "2.10.6"
+
+loop = None
+
+if Config.STRING_SESSION:
+    session = StringSession(str(Config.STRING_SESSION))
+else:
+    session = "zelzal"
+
+try:
+    zedub = ZedUserBotClient(
+        session=session,
+        api_id=Config.APP_ID,
+        api_hash=Config.API_HASH,
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(f"STRING_SESSION - {e}")
+    sys.exit()
+
+
+zedub.tgbot = tgbot = ZedUserBotClient(
+    session="ZedTgbot",
+    api_id=Config.APP_ID,
+    api_hash=Config.API_HASH,
+    loop=loop,
+    app_version=__version__,
+    connection=ConnectionTcpAbridged,
+    auto_reconnect=True,
+    connection_retries=None,
+).start(bot_token=Config.TG_BOT_TOKEN)
