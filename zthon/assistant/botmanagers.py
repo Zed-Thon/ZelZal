e1522c818723dfeb11a0bbc2b4449384561ea8fd
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-27 19:47:28 +0300

    Update botmanagers.py

--- zthon/assistant/botmanagers.py
+++ zthon/assistant/botmanagers.py
@@ -3,7 +3,7 @@ from math import floor
 
 from telethon.utils import get_display_name
 
-from userbot import zedub
+from zthon import zedub
 
 from ..Config import Config
 from ..core.logger import logging
