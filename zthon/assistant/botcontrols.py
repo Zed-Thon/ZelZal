7eae5ecb4c335c4cd0815416121832c7e124ff9d
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-05-27 19:54:33 +0300

    Update botcontrols.py

--- zthon/assistant/botcontrols.py
+++ zthon/assistant/botcontrols.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from telethon.errors import BadRequestError, FloodWaitError, ForbiddenError
 
-from userbot import zedub
+from zthon import zedub
 
 from ..Config import Config
 from ..core.logger import logging
