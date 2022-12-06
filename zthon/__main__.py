06c2af4eeb9d955ee2205795c3b5b37628f34a05
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2022-12-06 17:39:33 +0300

    Create __main__.py

---
+++ zthon/__main__.py
@@ -0,0 +1,77 @@
+import contextlib
+import sys
+
+import zthon
+from zthon import BOTLOG_CHATID, PM_LOGGER_GROUP_ID
+
+from .Config import Config
+from .core.logger import logging
+from .core.session import zedub
+from .utils import mybot
+from .utils import (
+    add_bot_to_logger_group,
+    load_plugins,
+    saves,
+    setup_bot,
+    startupmessage,
+    verifyLoggerGroup,
+)
+
+LOGS = logging.getLogger("Zelzal")
+cmdhr = Config.COMMAND_HAND_LER
+
+print(zthon.__copyright__)
+print(f"المرخصة بموجب شروط  {zthon.__license__}")
+
+cmdhr = Config.COMMAND_HAND_LER
+
+try:
+    LOGS.info("⌭ بـدء تنزيـل زدثــون ⌭")
+    zedub.loop.run_until_complete(setup_bot())
+    LOGS.info("⌭ بـدء تشغيـل البـوت ⌭")
+except Exception as e:
+    LOGS.error(f"{e}")
+    sys.exit()
+
+
+try:
+    LOGS.info("⌭ جـار تفعيـل وضـع الانـلاين ⌭")
+    zedub.loop.run_until_complete(mybot())
+    LOGS.info("✓ تـم تفعيـل الانـلاين .. بـنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"- {e}")
+
+
+try:
+    LOGS.info("⌭ جـاري تحميـل الملحقـات ⌭")
+    zedub.loop.create_task(saves())
+    LOGS.info("✓ تـم تحميـل الملحقـات .. بنجـاح ✓")
+except Exception as e:
+    LOGS.error(f"- {e}")
+
+
+async def startup_process():
+    await verifyLoggerGroup()
+    await load_plugins("plugins")
+    await load_plugins("assistant")
+    print("➖➖➖➖➖ 𝗭𝗧𝗵𝗼𝗻™ ➖➖➖➖➖")
+    print("تـم التنصـيب .. بنجـاح ✓")
+    print(
+        f"⌔┊تـم تنصيـب زدثــون يـوزربـوت . . بنجـاح 🧸♥️ \n\n⌔┊تحيـاتي .. زلــزال الهيبــه\n⌔┊قنـاة السـورس ↶.\n🌐┊@ZedThon "
+    )
+    print("➖➖➖➖➖ 𝗭𝗧𝗵𝗼𝗻™ ➖➖➖➖➖")
+    await verifyLoggerGroup()
+    await add_bot_to_logger_group(BOTLOG_CHATID)
+    if PM_LOGGER_GROUP_ID != -100:
+        await add_bot_to_logger_group(PM_LOGGER_GROUP_ID)
+    await startupmessage()
+    return
+
+
+zedub.loop.run_until_complete(startup_process())
+
+if len(sys.argv) in {1, 3, 4}:
+    with contextlib.suppress(ConnectionError):
+        zedub.run_until_disconnected()
+else:
+    zedub.disconnect()
