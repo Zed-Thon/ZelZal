fcca58eb8867f9e4c5ee8211381adaa4cb0e1792
Zein <122434261+zeinn1@users.noreply.github.com>
2023-02-26 23:49:41 +0200

    Update session.py

--- zthon/core/session.py
+++ zthon/core/session.py
@@ -7,7 +7,7 @@ from .bothseesion import bothseesion
 from .client import ZedUserBotClient
 from .logger import logging
 
-LOGS = logging.getLogger("زدثــون")
+LOGS = logging.getLogger("زين")
 __version__ = "2.10.6"
 
 loop = None
