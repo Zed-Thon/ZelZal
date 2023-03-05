ca18541f27b08746b632d579f2e5ed3f3d80296d
Zeinn0 <127055306+Zeinn0@users.noreply.github.com>
2023-03-06 00:36:16 +0200

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -31,7 +31,7 @@ class Config(object):
     TZ = os.environ.get("TZ", "Asia/Baghdad")
     # set this with required zed repo link
     UPSTREAM_REPO = os.environ.get(
-        "UPSTREAM_REPO", "https://github.com/Zeinn1/Zelzal"
+        "UPSTREAM_REPO", "https://github.com/Zeinn0/Zelzal"
     )
     # External plugins repo
     EXTERNAL_REPO = os.environ.get("EXTERNAL_REPO", None)
