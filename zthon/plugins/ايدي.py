f1bc6dd4ab07205e12422b080f55cf76ddd32829
Zeinn0 <127055306+Zeinn0@users.noreply.github.com>
2023-03-06 23:05:22 +0200

    Update ايدي.py

--- zthon/plugins/ايدي.py
+++ zthon/plugins/ايدي.py
@@ -36,7 +36,7 @@ from . import spamwatch
 
 plugin_category = "العروض"
 LOGS = logging.getLogger(__name__)
-ZED_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "•⎚• مـعلومـات المسـتخـدم مـن بـوت ڪـ‌๋‏ـرسـ‌๋‏ـتـ‌๋‏ـيـ‌๋‏ـن"
+ZED_TEXT = gvarstatus("CUSTOM_ALIVE_TEXT") or "•⎚• مـعلومـات المسـتخـدم مـن بـوت ڤي֠ــ‌ۢـ‌ٰـن֠ــ‌ۢـ‌ٰـﯛ‌م"
 ZEDM = gvarstatus("CUSTOM_ALIVE_EMOJI") or "⎉ "
 ZEDF = gvarstatus("CUSTOM_ALIVE_FONT") or "⋆⎈•━━⊶𐂡•𝖛𝖊𝖓𝖔𝖒 𝖚𝖘𝖊𝖗𝖇𝖔𝖙•𐂡⊷━━•⎈⋆"
 
