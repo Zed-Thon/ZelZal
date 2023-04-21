7d873ac9572a2d7e651825ad5620e6ba196932c0
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-04-21 22:14:16 +0300

    Update pluginManager.py

--- Zara/core/pluginManager.py
+++ Zara/core/pluginManager.py
@@ -17,8 +17,8 @@ github_patern = re.compile(r"(?:https?)?(?:www.)?(?:github.com/)?([\w\-.]+/[\w\-
 github_raw_pattern = re.compile(
     r"(?:https?)?(?:raw.)?(?:githubusercontent.com/)?([\w\-.]+/[\w\-.]+)/?"
 )
-trees_pattern = "https://api.github.com/repos/{}/git/trees/master"
-raw_pattern = "https://raw.githubusercontent.com/{}/master/{}"
+trees_pattern = "https://api.github.com/repos/{}/git/trees/Zara"
+raw_pattern = "https://raw.githubusercontent.com/{}/Zara/{}"
 
 LOGS = logging.getLogger(__name__)
 
