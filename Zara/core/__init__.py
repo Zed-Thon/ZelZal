ba21ccc3861c34df577bc5498078280a27e07e68
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-04-23 19:44:44 +0300

    Update __init__.py

--- Zara/core/__init__.py
+++ Zara/core/__init__.py
@@ -1,11 +1,4 @@
 from .decorators import check_owner
-from aiohttp import web
-from .route import routes
-
-async def web_server():
-    web_app = web.Application(client_max_size=30000000)
-    web_app.add_routes(routes)
-    return web_app
 
 CMD_INFO = {}
 PLG_INFO = {}
