02a05fae17693ae5e772b7342bbd445738f6e7b2
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-04-23 23:32:11 +0300

    Update __main__.py

--- Zara/__main__.py
+++ Zara/__main__.py
@@ -62,11 +62,6 @@ async def startup_process():
     await startupmessage()
     return
 
-async def externalrepo():
-    if Config.VCMODE:
-        await install_externalrepo("https://github.com/Zed-Thon/ZVCPlayer", "zvcplayer", "ZedVC")
-
-zedub.loop.run_until_complete(externalrepo())
 zedub.loop.run_until_complete(startup_process())
 
 if len(sys.argv) in {1, 3, 4}:
