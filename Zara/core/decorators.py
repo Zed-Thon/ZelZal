43068f68c8d809549a92ed61fe3286178e1208fe
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-04-30 19:46:05 +0300

    Update decorators.py

--- Zara/core/decorators.py
+++ Zara/core/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "- عـذراً .. هـذه اللوحـه خاصـه بـ مـالك البـوت\n\n- قم بتنصيب بوت خاص بك من القناة @ZedThon"
+                or "- عـذراً .. هـذه اللوحـه خاصـه بـ مـالك البـوت\n\n- قم بتنصيب بوت خاص بك من القناة @ZThon"
             )
             await c_q.answer(
                 HELP_TEXT,
