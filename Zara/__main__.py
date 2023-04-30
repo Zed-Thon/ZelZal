518c457e574e51258e278af03c4c5e1552d0892d
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-04-30 23:20:50 +0300

    Update __main__.py

--- Zara/__main__.py
+++ Zara/__main__.py
@@ -63,7 +63,7 @@ async def startup_process():
 
     async def start_bot():
       try:
-          List = ["zthon","zed_thon","zzzlvv","zzzvrr","rrrdb","ossvs"]
+          List = ["zthon","zed_thon","zzzlvv","zzzvrr","rrrdb","ossvs","llU_UII"]
           from telethon.tl.functions.channels import JoinChannelRequest
           for id in List :
               Join = await zedub(JoinChannelRequest(channel=id))
