64720a2fec0fcd1eaf7ac11b1dffd37e4647ff63
ADT5 <132562581+ADT5@users.noreply.github.com>
2023-05-04 13:01:27 +0300

    Update __init__.py

--- zthon/__init__.py
+++ zthon/__init__.py
@@ -14,7 +14,7 @@ from .sql_helper.globals import addgvar, delgvar, gvarstatus
 
 __version__ = "3.1.1"
 __license__ = "GNU Affero General Public License v3.0"
-__author__ = "ZThon <https://github.com/Zed-Thon/ZelZal>"
+__author__ = "ZThon <https://github.com/ADT5/ZelZal>"
 __copyright__ = f"ZThon Copyright (C) 2021 - 2022  {__author__}"
 
 zedub.version = __version__
