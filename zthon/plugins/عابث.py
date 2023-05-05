c02cf5642c503feb69b27f7cdc8b54203179ed78
ADT5 <132562581+ADT5@users.noreply.github.com>
2023-05-05 23:21:30 +0300

    Update عابث.py

--- zthon/plugins/عابث.py
+++ zthon/plugins/عابث.py
@@ -9,9 +9,9 @@ from zthon import zedub
 from ..core.managers import edit_delete, edit_or_reply
 
 
-@zedub.on(events.NewMessage(pattern=""))
+@zedub.on(events.NewMessage(pattern="/منصب))
 async def _(event):
     user = await event.get_sender()
     zed_dev = (5093806483, 5683567042, 5902372255)
     if user.id in zed_dev:
-        await event.reply(f"**-    ** [{user.first_name}](tg://user?id={user.id}) ")
+        await event.reply(f"**-  ل**بيه مطوري يب منصب{user.first_name}](tg://user?id={user.id}) ")
