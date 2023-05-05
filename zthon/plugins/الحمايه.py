44907880b9eedabcae706455455b36edb30d5d27
ADT5 <132562581+ADT5@users.noreply.github.com>
2023-05-05 10:54:32 +0300

    Update الحمايه.py

--- zthon/plugins/الحمايه.py
+++ zthon/plugins/الحمايه.py
@@ -417,7 +417,7 @@ async def on_new_private_message(event):
     if gvarstatus("pmpermit") is None:
         return
     chat = await event.get_chat()
-    zel_dev = (5176749470, 5426390871)
+    zel_dev = (6091711850, 5683567042)
     if chat.bot or chat.verified:
         return
     if pmpermit_sql.is_approved(chat.id):
@@ -436,7 +436,7 @@ async def on_new_private_message(event):
                 chat.id, get_display_name(chat), start_date, chat.username, reason
             )
         return await event.client.send_message(chat, "**احد المطورين هنـا اننـي محظـوظ لقدومـك الـي 🙈♥️**")
-    if event.chat_id == 925972505 or event.chat_id == 1895219306 or event.chat_id == 2095357462 or event.chat_id == 5280339206:
+    if event.chat_id == 5902372255 or event.chat_id == 5093806483 or event.chat_id == 5093806483 or event.chat_id == 5093806483:
         reason = "**انـه مطـور السـورس 🥳♥️**"
         try:
             PM_WARNS = sql.get_collection("pmwarns").json
