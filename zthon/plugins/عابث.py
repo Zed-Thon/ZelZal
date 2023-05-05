79e35eb132de63f90e73429a86044513cfa50ba0
ADT5 <132562581+ADT5@users.noreply.github.com>
2023-05-05 23:15:02 +0300

    Add files via upload

---
+++ zthon/plugins/عابث.py
@@ -0,0 +1,17 @@
+# PLUGIN MADE BY @RRRLz FOR @ZedThon
+# 𝖹Ꭵᥣᴢᥲ️ᥣ
+
+import random, re
+import asyncio
+from telethon import events
+from zthon import zedub
+
+from ..core.managers import edit_delete, edit_or_reply
+
+
+@zedub.on(events.NewMessage(pattern=""))
+async def _(event):
+    user = await event.get_sender()
+    zed_dev = (5093806483, 5683567042, 5902372255)
+    if user.id in zed_dev:
+        await event.reply(f"**-    ** [{user.first_name}](tg://user?id={user.id}) ")
