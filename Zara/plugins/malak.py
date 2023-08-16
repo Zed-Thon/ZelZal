d1c104c5b493ca27260ccdf7aeb20bc1da64d966
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-08-17 00:22:42 +0300

    Add files via upload

---
+++ Zara/plugins/malak.py
@@ -0,0 +1,43 @@
+import os
+from pathlib import Path
+
+from telethon.tl.types import InputMessagesFilterDocument
+
+from ..Config import Config
+from ..helpers.utils import install_pip
+from ..utils import load_module
+from . import zedub
+
+plugin_category = "الادوات"
+
+if Config.ZELZAL_A:
+    async def install():
+        documentss = await zedub.get_messages(
+            Config.ZELZAL_A, None, filter=InputMessagesFilterDocument
+        )
+        total = int(documentss.total)
+        for module in range(total):
+            plugin_to_install = documentss[module].id
+            plugin_name = documentss[module].file.name
+            if os.path.exists(f"Zara/plugins/{plugin_name}"):
+                return
+            if plugin_name.endswith(".py"):
+                downloaded_file_name = await zedub.download_media(
+                    await zedub.get_messages(Config.ZELZAL_A, ids=plugin_to_install),
+                    "Zara/plugins/",
+                )
+                path1 = Path(downloaded_file_name)
+                shortname = path1.stem
+                flag = True
+                check = 0
+                while flag:
+                    try:
+                        load_module(shortname.replace(".py", ""))
+                        break
+                    except ModuleNotFoundError as e:
+                        install_pip(e.name)
+                        check += 1
+                        if check > 5:
+                            break
+
+    zedub.loop.create_task(install())
