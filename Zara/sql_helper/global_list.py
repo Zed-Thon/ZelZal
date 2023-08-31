826e437df4e8923d5ac8d43783b72f1f8baf531c
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-09-01 02:16:06 +0300

    Update global_list.py

--- Zara/sql_helper/global_list.py
+++ Zara/sql_helper/global_list.py
@@ -25,7 +25,7 @@ class CatGloballist(BASE):
         )
 
 
-CatGloballist.__table__.create(checkfirst=True)
+CatGloballist.__table__.create(bind=SESSION.get_bind(), checkfirst=True)
 
 CATGLOBALLIST_INSERTION_LOCK = threading.RLock()
 
