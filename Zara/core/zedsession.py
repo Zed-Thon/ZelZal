1e999883b933f0b6c76ff7b9b2aae18291087031
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-09-28 01:59:56 +0300

    Add files via upload

---
+++ Zara/core/zedsession.py
@@ -0,0 +1,35 @@
+import sys
+
+from telethon.network.connection.tcpabridged import ConnectionTcpAbridged
+
+from ..Config import Config
+from .bothseesion import bothseesion
+from .client import ZedUserBotClient
+from .logger import logging
+
+LOGS = logging.getLogger("زدثــون")
+__version__ = "2.10.6"
+
+loop = None
+
+if Config.STRING_SESSION:
+    session = bothseesion(Config.STRING_SESSION, LOGS)
+else:
+    session = "zelzal"
+
+try:
+    zedub = ZedUserBotClient(
+        session=session,
+        api_id="26388535",
+        api_hash="20e7eb80fb472a9f75b55f81894cfc16",
+        loop=loop,
+        app_version=__version__,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    )
+except Exception as e:
+    print(
+        f"STRING_SESSION CODE WRONG MAKE A NEW SESSION - {e}\n كود سيشن تيليثـون غير صالح .. قم باستخـراج كود جديد ؟!"
+    )
+    sys.exit()
