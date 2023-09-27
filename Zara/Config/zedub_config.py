5874cb6f37b836fb9d6105c1f5d642751dbf425b
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-09-28 01:27:23 +0300

    Update zedub_config.py

--- Zara/Config/zedub_config.py
+++ Zara/Config/zedub_config.py
@@ -2,9 +2,9 @@
 
 import os
 
-ENV = bool(os.environ.get("ENV", False))
+#ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    from sample_config import Config  # noqa
-elif os.path.exists("config.py"):
-    from config import Development as Config  # noqa
+#if ENV:
+from sample_config import Config  # noqa
+#elif os.path.exists("config.py"):
+    #from config import Development as Config  # noqa
