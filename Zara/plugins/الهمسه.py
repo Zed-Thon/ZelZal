fc47783d16d28beb93182d33688c8f586997fa57
Zed-Thon <104465413+Zed-Thon@users.noreply.github.com>
2023-10-05 23:11:00 +0300

    Update الهمسه.py

--- Zara/plugins/الهمسه.py
+++ Zara/plugins/الهمسه.py
@@ -8,15 +8,21 @@ async def wspr(event):
     if event.fwd_from:
         return
     input_str = event.pattern_match.group(1)
-    wspr_bot = "@nnbbot"
+    wspr_bot = "@BYYiBoT"
     if event.reply_to_msg_id:
         reply_to_id = await event.get_reply_message()
     ton = await bot.inline_query(wspr_bot, input_str) 
     await ton[0].click(event.chat_id)
     await event.delete()
     
-@borg.on(admin_cmd("الهمسه"))
-async def _(event):
-     if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
-        await event.edit("**- شـرح كتابة همسـه سـرية :\n⋆┄─┄─┄─┄┄─┄─┄─┄─┄┄⋆\n- ارسـل الامـر  (** .همسه + الرسالة + معرف الشخص **)\n- مثـــال :**\n`.همسه هلو @zzzzl1l`")
-        
+@borg.on(admin_cmd(pattern="اهمس ?(.*)"))
+async def wspr(event):
+    if event.fwd_from:
+        return
+    input_str = event.pattern_match.group(1)
+    wspr_bot = "@BYYiBoT"
+    if event.reply_to_msg_id:
+        reply_to_id = await event.get_reply_message()
+    ton = await bot.inline_query(wspr_bot, input_str) 
+    await ton[0].click(event.chat_id)
+    await event.delete()
